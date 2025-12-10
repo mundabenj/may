2df49fe859a480fa26cff07879c96d6423fdf5a8
ALPHAWOLF\munda <benjamin.mundama@strathmore.edu>
2025-12-10 14:34:08 +0300

    restore html

--- query.sql
+++ query.sql
@@ -1,12 +1,13 @@
 queries
 -- Query to select all columns from the 'users' table
 
-SELECT u.fullname, u.email, u.points, r.role, g.gender, GROUP_CONCAT(s.skill SEPARATOR ', ') AS skills, SUM(s.points) AS sum_skill_points
+SELECT distinct(u.userId), u.fullname, u.email, up.points, r.role, g.gender, GROUP_CONCAT(s.skill SEPARATOR ', ') AS skills, SUM(s.points) AS sum_skill_points
 FROM users u
 LEFT JOIN roles r USING(roleId)
 LEFT JOIN gender g USING(genderId)
 LEFT JOIN user_skills us USING(userId)
+LEFT JOIN user_points up USING(userId)
 LEFT JOIN skills s USING(skillId)
 GROUP BY us.userId
-HAVING COUNT(us.userId) > 0
+HAVING COUNT(us.userId) > 1
 ORDER BY u.fullname;
